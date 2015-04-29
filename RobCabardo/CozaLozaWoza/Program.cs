b50fc5d94fd9bb3db320403a43c4b3b687bcf0cb
Rob Cabardo <robcabardo@gmail.com>
2015-04-29 14:49:53 -0500

    Write program

--- RobCabardo/CozaLozaWoza/Program.cs
+++ RobCabardo/CozaLozaWoza/Program.cs
@@ -10,6 +10,56 @@ namespace CozaLozaWoza
     {
         static void Main(string[] args)
         {
+            int count = 1;
+
+            for (int i = 1; i < 111; i++)
+            {
+                if (count < 12)
+                {
+                    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                        Console.Write("CozaLozaWoza ");
+                    else if (i % 3 == 0 && i % 5 == 0)
+                        Console.Write("CozaLoza ");
+                    else if (i % 3 == 0 && i % 7 == 0)
+                        Console.Write("CozaWoza ");
+                    else if (i % 5 == 0 && i % 7 == 0)
+                        Console.Write("LozaWoza ");
+                    else
+                        if (i % 3 == 0)
+                            Console.Write("Coza ");
+                        else if (i % 5 == 0)
+                            Console.Write("Loza ");
+                        else if (i % 7 == 0)
+                            Console.Write("Woza ");
+                        else
+                            Console.Write(i + " ");
+                }
+                else
+                {
+                    count = 1;                                          // reset count per line
+                    Console.WriteLine();                                // create new line
+
+                    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                        Console.Write("CozaLozaWoza ");
+                    else if (i % 3 == 0 && i % 5 == 0)
+                        Console.Write("CozaLoza ");
+                    else if (i % 3 == 0 && i % 7 == 0)
+                        Console.Write("CozaWoza ");
+                    else if (i % 5 == 0 && i % 7 == 0)
+                        Console.Write("LozaWoza ");
+                    else
+                        if (i % 3 == 0)
+                            Console.Write("Coza ");
+                        else if (i % 5 == 0)
+                            Console.Write("Loza ");
+                        else if (i % 7 == 0)
+                            Console.Write("Woza ");
+                        else
+                            Console.Write(i + " ");
+                }
+                count++;
+            }
+            Console.ReadLine();
         }
     }
 }
